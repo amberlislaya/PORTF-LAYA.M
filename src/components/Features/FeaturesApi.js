e3e39e59a97a1cf9ac5c7ea5f73f03a6964ca375
amberlislaya <laya.playapps@gmail.com>
2023-07-30 00:13:02 -0300

    contenido

--- src/components/Features/FeaturesApi.js
+++ src/components/Features/FeaturesApi.js
@@ -3,37 +3,37 @@ const data = [
     id: 1,
     image: "https://img.icons8.com/ios/64/000000/menu.png",
     title: "Estrategia de negocios",
-    desc: "Me tiro entre la hierba alta junto al arroyo mientras me acuesto cerca de la tierra.",
+    desc: "plan integral y a largo plazo diseñado para alcanzar los objetivos y metas de una empresa. Involucra la identificación de oportunidades, el análisis del entorno competitivo, la definición de ventajas competitivas, la asignación de recursos y la implementación de acciones para lograr una posición sólida en el mercado y generar valor para los clientes y accionistas. La estrategia guía la toma de decisiones y la dirección general de la empresa en busca de un crecimiento sostenible y una ventaja competitiva duradera.",
   },
   {
     id: 2,
     image: "https://img.icons8.com/ios/40/000000/open-book.png",
     title: "Desarrollo de aplicaciones",
-    desc: "Utiliza un diccionario de más de 200 palabras latinas, combinado con un puñado de oraciones modelo.",
+    desc: "proceso de creación, diseño y programación de software para dispositivos móviles, computadoras u otros dispositivos electrónicos. Implica la concepción de la idea, el diseño de la interfaz de usuario, la codificación del código fuente, la implementación de funcionalidades y la realización de pruebas para garantizar un funcionamiento óptimo y libre de errores. El objetivo es producir aplicaciones eficientes, seguras y atractivas que satisfagan las necesidades y deseos de los usuarios, ya sea para uso personal, empresarial o en línea.",
   },
   {
     id: 3,
     image: "https://img.icons8.com/fluency-systems-regular/64/000000/retro-tv.png ",
     title: "App Development",
-    desc: "Me tiro entre la hierba alta junto al arroyo mientras me acuesto cerca de la tierra.",
+    desc: "desarrolladores de aplicaciones utilizan lenguajes de programación y marcos de desarrollo para construir aplicaciones funcionales y fáciles de usar que satisfagan necesidades y objetivos específicos. El objetivo es entregar aplicaciones de alta calidad, seguras y eficientes que mejoren las experiencias de los usuarios y brinden valor tanto a individuos como a empresas. A medida que la tecnología continúa evolucionando, el desarrollo de aplicaciones sigue siendo un campo dinámico con constantes innovaciones y oportunidades para que los desarrolladores creen soluciones impactantes para audiencias diversas.",
   },
   {
     id: 4,
     image: "https://img.icons8.com/material-outlined/64/000000/topic--v1.png ",
     title: "Aplicación movil",
-    desc: "Hay muchas variaciones de pasajes de Lorem Ipsum disponibles, pero la mayoría.",
+    desc: "Las aplicaciones móviles están diseñadas para realizar diversas funciones y satisfacer necesidades específicas de los usuarios, desde tareas prácticas como gestión de correo electrónico, navegación por mapas y comunicación con redes sociales, hasta entretenimiento, educación y productividad. Ofrecen una experiencia optimizada para dispositivos móviles, con interfaces adaptadas a pantallas táctiles y funcionalidades que aprovechan las capacidades del hardware del dispositivo.",
   },
   {
     id: 5,
     image: "https://img.icons8.com/ios-filled/64/000000/wifi.png ",
     title: "CEO Marketing",
-    desc: "siempre libre de repeticiones, humor inyectado o palabras no características, etc.",
+    desc: "El CEO de Marketing trabaja en estrecha colaboración con otros miembros del equipo directivo para alinear las estrategias de marketing con los objetivos generales del negocio. Supervisa el desarrollo e implementación de campañas publicitarias, estrategias de redes sociales, programas de relaciones públicas y esfuerzos de branding para asegurar que la empresa alcance a su audiencia de manera efectiva.",
   },
   {
     id: 6,
     image: "https://img.icons8.com/glyph-neue/50/000000/polyline.png ",
     title: "Portafolio Personal Abril",
-    desc: " Utiliza un diccionario de más de 200 palabras latinas, combinado con un puñado de oraciones modelo.",
+    desc: " En un portafolio personal, se incluyen elementos como proyectos creativos, trabajos artísticos, muestras de escritura, logros académicos, certificaciones profesionales y cualquier otra evidencia de habilidades relevantes. Además, puede contener una biografía, una declaración personal y enlaces a perfiles profesionales o en redes sociales para que los interesados puedan conocer más sobre el individuo",
   },
 ]
 export default data;
